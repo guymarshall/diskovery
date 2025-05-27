0ed02c918cd756b7a39e92d27ff351407b308af5
Guy Marshall <guymarshall9@gmail.com>
2025-05-27 15:33:13 +0100

    WIP: Fit buttons to size of window with padding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eframe::{CreationContext, NativeOptions, egui};
-use egui::Ui;
+use egui::{Button, Ui, vec2};
 
 struct MyApp {
     locations: Vec<&'static str>,
@@ -23,12 +23,32 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(context, |ui: &mut Ui| {
             ui.heading("Select a location to search:");
 
-            self.locations.iter().for_each(|location: &&str| {
-                if ui.button(*location).clicked() {
-                    println!("{} clicked", location);
-                    // TODO: navigate to the section for this location
-                }
-            });
+            let available_height: f32 = ui.available_height();
+            let button_count: usize = self.locations.len();
+            let padding: f32 = 16.0;
+            let button_spacing: f32 = 12.0;
+            let total_spacing: f32 = padding * 2.0 + button_spacing * (button_count as f32 - 1.0);
+            let button_height: f32 =
+                ((available_height - total_spacing) / button_count as f32).max(32.0);
+
+            ui.add_space(padding);
+
+            self.locations
+                .iter()
+                .enumerate()
+                .for_each(|(i, location): (usize, &&str)| {
+                    let button: Button<'_> =
+                        Button::new(*location).min_size(vec2(ui.available_width(), button_height));
+                    if ui.add(button).clicked() {
+                        println!("{} clicked", location);
+                        // TODO: navigate to the section for this location
+                    }
+                    if i < button_count - 1 {
+                        ui.add_space(button_spacing);
+                    }
+                });
+
+            ui.add_space(padding);
         });
     }
 }
